93f913eccab30a3f33a7c0912e2897700b76fe86
Rick White <rickatnight11@gmail.com>
2011-03-31 21:17:00 -0400

    Added OptionParser support

--- srcdsm.py
+++ srcdsm.py
@@ -3,29 +3,79 @@
 by Rick White (rickatnight11@gmail.com)
 """
 
-_debug=True
-
+#Imports
 import ConfigParser
+from optparse import OptionParser
 
 #Variables
-config_file="srcds.conf"
+_debug = False
+_config_file="srcdsm.conf"
+_default_install_dir = '/opt/srcds'
 users=[]
 game_types=[]
 servers=[]
 processes=[]
 
-def ReadConf():
+
+def ProcessArgs():
+    global _debug
     
+    #Define option/argument parser
+    parser = OptionParser(usage='usage: %prog [OPTION]', version='%prog 0.1')
+    parser.add_option('-d', '--deploy', action='store_true', dest='deploy', help='deploy new installation of SRCDS', default=False)
+    parser.add_option('-i', '--install', action='store_true', dest='install', help='install new instance of a game', default=False)
+    parser.add_option('-s', '--start', action='store_true', dest='start', help='start a game', default=False)
+    parser.add_option('-e', '--stop', action='store_true', dest='stop', help='stop a game', default=False)
+    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='increase verbosity (enable debug mode)', default=False)
+    
+        
+    (options, args) = parser.parse_args()
+    
+    #Check for extra arguments
+    if len(args) > 0:
+        parser.error("too many arguments passed")
+    
+    #Check for verbosity/debug mode
+    if options.verbose:
+        _debug = True
+    
+    #Check for mutually exclusive 'action' switches
+    actionlist = []
+    if options.deploy:
+        actionlist.append('--deploy')
+    if options.install:
+        actionlist.append('--install')
+    if options.start:
+        actionlist.append('--start')
+    if options.stop:
+        actionlist.append('--stop')
+    
+    if len(actionlist) > 1:
+        error_message = 'Cannot use more than one action switch at a time ('
+        index = 0
+        for action in actionlist:
+            if index > 0:
+                error_message += ', '
+            error_message += action
+            index += 1
+        error_message += ')'
+        parser.error(error_message)
+    
+    
+
+
+def ReadConf():
     #Read file into ConfigParser
     try:
-        config = open(config_file, 'r')
+        config = open(_config_file, 'r')
         config = ConfigParser.SafeConfigParser()
         config = ConfigParser.SafeConfigParser()
-        config.read(config_file)
+        config.read(_config_file)
     except IOError:
-        print 'cannot open', config_file
+        print 'cannot open', _config_file
+        return False
     else:
-        if _debug: print "Found Config File (" + config_file + ")"
+        if _debug: print "Found Config File (" + _config_file + ")"
     
     
     #Load default variables
@@ -55,9 +105,9 @@ def ReadConf():
         if server[:7] == "server-":
             servers.append(dict(config.items(server)))
     
-    print servers
+    #print servers
     
     return 0
 
-
+ProcessArgs()
 ReadConf()
