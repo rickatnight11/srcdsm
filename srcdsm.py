1e0b1043f898abd826dda1d1fa951e0b3ddacd35
Rick White <rickatnight11@gmail.com>
2011-09-02 00:24:58 -0400

    Rearchitecture options and config files
    Establish message verbosity system that allows methods to set the
    severity of messages, which is compared against the provided verbosity
    level.  Eventually I may switch to an existing logging library, which
    will be way better than anything I can write.
    
    - Optional switches set _verbosity
    
    	- --verbose option sets _verbosity to 2 (noisy)
    	- --quiet option sets _verbosity to 0 (silent)
    	- Otherwise _verbosity defaults to 1
    
    - PrintMessage method prints messages based on severity as it compares
    to _verbosity
    
    Configuration architecture reworked.
    
    	- srcdsm.conf now contains only srcds settings and defaults
    	- deployments.conf contains list of installed deployments
    	- gamtypes directory contains list of gametype configs, one for each
    gametype, which contain settings and defaults for that specific gametype
    
    Change-Id: If4c88bee9048546188d7e508dce10d86c4595404

--- srcdsm.py
+++ srcdsm.py
@@ -2,31 +2,57 @@
 """ srcds Server Manager
 by Rick White (rickatnight11@gmail.com)
 """
-
-#Imports
+"""
+Imports
+"""
 import ConfigParser
+import os, os.path
+import sys
 from optparse import OptionParser
 
-#Variables
-_debug = False
-_config_file="srcdsm.conf"
-_default_install_dir = '/opt/srcds'
-users=[]
-game_types=[]
-servers=[]
-processes=[]
+"""
+Global Variables
+"""
+#Verbosity can be 0 (silent), 1 (default), or 2 (noisy)
+_verbosity = 1
 
+#Location of srcdsm config file (default is /etc/srcdsm/srcdsm.conf)
+_config_file='/etc/srcdsm/srcdsm.conf'
+_config_file = os.path.normpath(_config_file)
+
+#Location of deployments config file (populated by config file)
+_deployments_conf= ''
+
+#Directory to deploy srcds installations, if not specified (populated by config file)
+_default_install_dir = ''
+
+#Directory containing gametype configs (populated by config file)
+_gametypes_dir = ''
+
+#List of known game types
+game_types=[]
+
+#List of installed deployments
+deployments=[]
+
+#List of installed instances 
+instances=[]
+
+"""
+Logic
+"""
 
 def ProcessArgs():
-    global _debug
+    global _verbosity
     
     #Define option/argument parser
-    parser = OptionParser(usage='usage: %prog [OPTION]', version='%prog 0.1')
+    parser = OptionParser(usage='usage: %prog [OPTIONS]', version='%prog 0.1')
     parser.add_option('-d', '--deploy', action='store_true', dest='deploy', help='deploy new installation of SRCDS', default=False)
     parser.add_option('-i', '--install', action='store_true', dest='install', help='install new instance of a game', default=False)
     parser.add_option('-s', '--start', action='store_true', dest='start', help='start a game', default=False)
     parser.add_option('-e', '--stop', action='store_true', dest='stop', help='stop a game', default=False)
     parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='increase verbosity (enable debug mode)', default=False)
+    parser.add_option('-q', '--quiet', action='store_true', dest='quiet', help='decrease verbosity (enable silent mode)', default=False)
     
         
     (options, args) = parser.parse_args()
@@ -35,9 +61,13 @@ def ProcessArgs():
     if len(args) > 0:
         parser.error("too many arguments passed")
     
-    #Check for verbosity/debug mode
-    if options.verbose:
-        _debug = True
+    #Check for verbosity switches
+    if options.verbose and options.quiet:
+        PrintMessage('Both --verbose and --quiet switches used.  Defaulting to normal verbosity', 2)
+    elif options.verbose:
+        _verbosity = 2
+    elif options.quiet:
+        _verbosity = 0
     
     #Check for mutually exclusive 'action' switches
     actionlist = []
@@ -50,6 +80,7 @@ def ProcessArgs():
     if options.stop:
         actionlist.append('--stop')
     
+    #List superfluous action switches used
     if len(actionlist) > 1:
         error_message = 'Cannot use more than one action switch at a time ('
         index = 0
@@ -65,49 +96,61 @@ def ProcessArgs():
 
 
 def ReadConf():
-    #Read file into ConfigParser
-    try:
-        config = open(_config_file, 'r')
-        config = ConfigParser.SafeConfigParser()
-        config = ConfigParser.SafeConfigParser()
-        config.read(_config_file)
-    except IOError:
-        print 'cannot open', _config_file
+    #Check existence and permissions of config file
+    if os.path.isfile(_config_file) and os.R_OK(_config_file):
+        
+        #Parse config file
+        try:
+            config = ConfigParser.SafeConfigParser()
+            config.read(_config_file)
+        except IOError:
+            PrintMessage('cannot open' + _config_file, 0)
+            return False
+        else:
+            PrintMessage('Loaded Config File (' + _config_file + ')', 3)
+    elif not os.path.isfile(_config_file):
+        PrintMessage(_config_file + ' does not exist!', 0)
+        return False
+    elif not os.R_OK(_config_file):
+        PrintMessage('Insufficient permissions to read ' + _config_file + '!', 0)
         return False
     else:
-        if _debug: print "Found Config File (" + _config_file + ")"
-    
+        PrintMessage('Unknown error attempting to load config file!', 0)
+        return False
     
     #Load default variables
-    default_items = dict(config.items("DEFAULT"))
+    defaults = dict(config.items('settings'))
+    if defaults.has_key('_default_install_dir'):
+        global _default_install_dir
+        _default_install_dir = os.path.normpath(defauts['_default_install_dir'])
+        PrintMessage('Default installation directory is ' + _default_install_dir, 2)
+    else:
+        PrintMessage('Default installation directory missing from config file!', 1)
     
-    
-    #Add global processes to processes list
-    global_procs = default_items['global_processes'].split(',')
-    for proc in global_procs:
-        processes.append(proc)
-    if _debug: print "Added (" + str(len(global_procs)) + ") processes from global_processes."
-    
-    #Add global users to users list
-    global_users= default_items['global_users'].split(',')
-    for user in global_users:
-        users.append(user)   
-    if _debug: print "Added (" + str(len(global_users)) + ") users from global_users."
-    
-    #Add games-types to game-types list
-    for game_type in config.sections():
-        if game_type[:10] == "game-type-":
-            game_types.append(dict(config.items(game_type)))
+    if defaults.has_key('_deployments_conf'):
+        global _deployments_conf
+        _deployments_conf = os.path.normpath(defaults['_deployments_conf'])
+        PrintMessage('Deployment config file is ' + _deployments_conf, 2)
+    else:
+        PrintMessage('Deployment config file missing from config file!', 0)
+        return False
 
+    if defaults.has_key('_gametypes_dir'):
+        global _gametypes_dir
+        _gametypes_dir = os.path.normpath(defaults['_gametypes_dir'])
+        PrintMessage('Gametypes config directory is ' + _gametypes_dir, 2)
+    else:
+        PrintMessage('Gametypes config directory missing from config file!', 0)
+        return False
     
-    #Add servers to servers list
-    for server in config.sections():
-        if server[:7] == "server-":
-            servers.append(dict(config.items(server)))
-    
-    #print servers
-    
-    return 0
+    return True
 
+def PrintMessage(message, severity):
+    if severity > _verbosity:
+        pass
+    else:
+        print message
+    
 ProcessArgs()
-ReadConf()
+if not ReadConf():
+    sys.exit[1]
